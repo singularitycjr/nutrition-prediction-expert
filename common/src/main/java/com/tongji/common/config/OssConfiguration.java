63ce6257002e73df1015ad4ce833938db7e8fea6
jixiangyuyu <2393063097@qq.com>
2023-10-18 14:15:06 +0800

    整合阿里云OSS

---
+++ common/src/main/java/com/tongji/common/config/OssConfiguration.java
@@ -0,0 +1,24 @@
+package com.tongji.common.config;
+
+
+import com.tongji.common.properties.AliOssProperties;
+import com.tongji.common.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
+        log.info("初始化阿里云OSS工具类, {}", aliOssProperties);;
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
