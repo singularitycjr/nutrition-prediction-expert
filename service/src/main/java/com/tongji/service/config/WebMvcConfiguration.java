0b4797e1f1bcb90ff8f1f27a7895d8a26de0e60b
jixiangyuyu <2393063097@qq.com>
2023-10-18 15:52:46 +0800

    死都别用WebMvcConfigurationSupport，坑壁玩意

--- service/src/main/java/com/tongji/service/config/WebMvcConfiguration.java
+++ service/src/main/java/com/tongji/service/config/WebMvcConfiguration.java
@@ -5,19 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 @Slf4j
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
     private TokenInterceptor tokenInterceptor;
 
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(this.tokenInterceptor)
                 .addPathPatterns("/**")
@@ -25,7 +25,6 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .excludePathPatterns("/user/register")
                 .excludePathPatterns("/v3/api-docs/**", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**");
     }
-
 
     ///**
     // * 日期格式标准化
