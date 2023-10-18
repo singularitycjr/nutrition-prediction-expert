070963c3424986d25bac92572b22460843939711
jixiangyuyu <2393063097@qq.com>
2023-10-18 09:50:13 +0800

    初始化工程

---
+++ src/main/java/com/tongji/nutritionalestimation/NutritionalEstimationApplication.java
@@ -0,0 +1,13 @@
+package com.tongji.nutritionalestimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionalEstimationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutritionalEstimationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tongji/nutritionalestimation/NutritionalEstimationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongji.nutritionalestimation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionalEstimationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
