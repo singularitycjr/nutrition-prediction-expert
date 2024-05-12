e2ee813440c4ba44f9615287f7d32b7dc6404ef9
Oscar Clayderman <906929332@qq.com>
2024-05-12 14:46:40 +0800

    update

--- service/doctor-service/src/main/java/com/tongji/doctor/controller/GlucoseController.java
+++ service/doctor-service/src/main/java/com/tongji/doctor/controller/GlucoseController.java
@@ -19,49 +19,49 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/glucose")
 @Tag(name = "血糖数据")
 public class GlucoseController {
-    @Autowired
-    GlucoseServiceImpl glucoseService;
-
-    @Operation(summary = "获取血糖数据")
-    @GetMapping("/getGlucose")
-    public ResponseResult getGlucose(TimeRangeDTO timeRangeDTO){
-        log.info("获取血糖数据: {}", timeRangeDTO);
-        return this.glucoseService.getGlucose(timeRangeDTO);
-    }
-
-    @Operation(summary = "删除血糖数据")
-    @DeleteMapping("/deleteGlucose/{id}")
-    public ResponseResult deleteGlucose(@PathVariable("id") Long id){
-        return this.glucoseService.deleteGlucose(id);
-    }
-
-    @Operation(summary = "修改血糖数据")
-    @PutMapping("/updateGlucose")
-    public ResponseResult updateGlucose(@RequestBody GlucoseDTO glucoseDTO){
-        return this.glucoseService.updateGlucose(glucoseDTO);
-    }
-
-    @Operation(summary = "添加血糖数据")
-    @PostMapping("/addGlucose")
-    public ResponseResult addGlucose(@RequestBody GlucoseAddDTO glucoseAddDTO){
-        return this.glucoseService.addGlucose(glucoseAddDTO);
-    }
-
-    @Operation(summary = "添加血糖数据文件")
-    @PostMapping("/uploadGlucoseFile")
-    public ResponseResult uploadGlucoseFile(@RequestParam("file") MultipartFile file){
-        return this.glucoseService.uploadGlucoseFile(file);
-    }
-
-    @Operation(summary = "上传血糖两列数据")
-    @PostMapping("/addFileData")
-    public ResponseResult addFileData(@RequestBody GlucoseFileAddDTO glucoseFileAddDTO){
-        return this.glucoseService.addFileData(glucoseFileAddDTO);
-    }
-
-    @Operation(summary = "调go接口获取血糖预测结果")
-    @GetMapping("/getPredictGlucose")
-    public ResponseResult getPredictGlucose(){
-        return this.glucoseService.getPredictGlucose();
-    }
+//    @Autowired
+//    GlucoseServiceImpl glucoseService;
+//
+//    @Operation(summary = "获取血糖数据")
+//    @GetMapping("/getGlucose")
+//    public ResponseResult getGlucose(TimeRangeDTO timeRangeDTO){
+//        log.info("获取血糖数据: {}", timeRangeDTO);
+//        return this.glucoseService.getGlucose(timeRangeDTO);
+//    }
+//
+//    @Operation(summary = "删除血糖数据")
+//    @DeleteMapping("/deleteGlucose/{id}")
+//    public ResponseResult deleteGlucose(@PathVariable("id") Long id){
+//        return this.glucoseService.deleteGlucose(id);
+//    }
+//
+//    @Operation(summary = "修改血糖数据")
+//    @PutMapping("/updateGlucose")
+//    public ResponseResult updateGlucose(@RequestBody GlucoseDTO glucoseDTO){
+//        return this.glucoseService.updateGlucose(glucoseDTO);
+//    }
+//
+//    @Operation(summary = "添加血糖数据")
+//    @PostMapping("/addGlucose")
+//    public ResponseResult addGlucose(@RequestBody GlucoseAddDTO glucoseAddDTO){
+//        return this.glucoseService.addGlucose(glucoseAddDTO);
+//    }
+//
+//    @Operation(summary = "添加血糖数据文件")
+//    @PostMapping("/uploadGlucoseFile")
+//    public ResponseResult uploadGlucoseFile(@RequestParam("file") MultipartFile file){
+//        return this.glucoseService.uploadGlucoseFile(file);
+//    }
+//
+//    @Operation(summary = "上传血糖两列数据")
+//    @PostMapping("/addFileData")
+//    public ResponseResult addFileData(@RequestBody GlucoseFileAddDTO glucoseFileAddDTO){
+//        return this.glucoseService.addFileData(glucoseFileAddDTO);
+//    }
+//
+//    @Operation(summary = "调go接口获取血糖预测结果")
+//    @GetMapping("/getPredictGlucose")
+//    public ResponseResult getPredictGlucose(){
+//        return this.glucoseService.getPredictGlucose();
+//    }
 }
